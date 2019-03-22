72a45c60e7e0d7403ae5012ffb8fd8b7a5ec91b5
Adam  Simon <33034441+adam09si@users.noreply.github.com>
2019-03-21 21:37:54 -0700

    fixed

--- extract_energies_geomoutputs.sh
+++ extract_energies_geomoutputs.sh
@@ -1,5 +1,9 @@
-for i in *.out; do
-	echo $i >> Results.txt
-	g09_getenergies.py $i >> Results.txt
-	echo "**********************" >> Results.txt
-done
+ #!/bin/bash
+
+ for i in *out; do
+     echo $i >> Results.txt
+     g09_getenergies.py $i >> Results.txt
+     echo "*******************" >> Results.txt
+ done
+
+ process_results.pl > Results_p.txt
